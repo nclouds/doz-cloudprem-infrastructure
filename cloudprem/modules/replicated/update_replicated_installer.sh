caa52fc79230a9e97db7c2440553f25bed1279d0
ronaldour <ronaldo@nclouds.com>
2021-01-12 16:22:13 -0500

    Added replicated.conf mount

--- cloudprem/modules/replicated/update_replicated_installer.sh
+++ cloudprem/modules/replicated/update_replicated_installer.sh
@@ -19,3 +19,6 @@ rm "$GENERATE_SCRIPT"
 yq e 'select(.kind == "Deployment").spec.template.spec.volumes += [{"name": "replicated-license", "secret": { "secretName": "{{ .Values.license_secret }}"}}]' -i charts/replicated/templates/replicated.yaml
 yq e '(select(.kind == "Deployment").spec.template.spec.containers[] | select(.name == "replicated").volumeMounts) += [{"name": "replicated-license", "mountPath": "/tmp/license.rli", "subPath": "license.rli"}]' -i charts/replicated/templates/replicated.yaml
 
+# Adding the config voumes
+yq e '(select(.kind == "Deployment").spec.template.spec.volumes[] | select(.name == "replicated-conf")) |= {"name": "replicated-conf", "configMap": { "name": "{{ .Values.replicated_conf }}"}}' -i charts/replicated/templates/replicated.yaml
+yq e '(select(.kind == "Deployment").spec.template.spec.containers[] | select(.name == "replicated").volumeMounts[] | select(.name == "replicated-conf")) |= {"name": "replicated-conf", "mountPath": "/host/etc/replicated.conf", "subPath": "replicated.conf"}' -i charts/replicated/templates/replicated.yaml
